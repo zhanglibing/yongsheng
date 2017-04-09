add31a333feff2661459abc6ee394a505c3bad15
zhanglibing <1053081179@qq.com>
2017-04-09 13:09:15 +0800

    ad

--- js/index.js
+++ js/index.js
@@ -14,4 +14,19 @@ $(function(){
             $(this).find('a').removeClass('active');
         }
     })
+
+    $('.head ul li').on('click',function(e){
+        e.preventDefault();
+        $('.project li').attr('data',$(this).attr('data'));
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+    })
+    var arr=['app','web','other'];
+    $('.project li').on('click',function(){
+       var index=$(this).index();
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+        var imgUrl='images/'+$(this).attr('data')+'-'+arr[index]+'.png';
+        $('.result li img').attr('src',imgUrl);
+    })
 })
