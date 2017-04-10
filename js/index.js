9c56244b8f6d34393b0b15b497e1c0ed0c75ef31
zhanglibing <1053081179@qq.com>
2017-04-10 22:10:07 +0800

    add

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(function(){
         $('.nav li').removeClass('active')
         $(this).addClass('active');
     })
+    
     var audio = $("audio").get(0);
     $('.nav-content .right .img').on('click',function(){
         $(this).toggleClass('active');
@@ -31,19 +32,21 @@ $(function(){
             $('.xinxi ul li').removeClass("active");
         }
     })
-
+    var index=0;
+    var arr=['app','web','other'];
     $('.head ul li').on('click',function(e){
         e.preventDefault();
         $('.project li').attr('data',$(this).attr('data'));
-        $(this).siblings().removeClass('active');
-        $(this).addClass('active');
-    })
-    var arr=['app','web','other'];
-    $('.project li').on('click',function(){
-       var index=$(this).index();
-        $(this).siblings().removeClass('active');
-        $(this).addClass('active');
-        var imgUrl='images/'+$(this).attr('data')+'-'+arr[index]+'.png';
+        $(this).addClass('active').siblings().removeClass('active');
+        var imgUrl='images/'+$(this).attr('data')+'-'+arr[index]+'.jpg';
         $('.result li img').attr('src',imgUrl);
+        return false;
+    })
+    $('.project li').on('click',function(){
+        index=$(this).index();
+        $(this).addClass('active').siblings().removeClass('active');;
+        var imgUrl='images/'+$(this).attr('data')+'-'+arr[index]+'.jpg';
+        $('.result li img').attr('src',imgUrl);
+        return false;
     })
 })
